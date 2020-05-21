ef947de4d544618d9db07136078e41b3f3b7a3ef
CelesQYZheng <celes.qy.zheng@gmail.com>
2020-05-21 19:53:28 +0800

    added new method

--- WebElementInterfaceMethodsProperties/UnitTest1.cs
+++ WebElementInterfaceMethodsProperties/UnitTest1.cs
@@ -56,5 +56,68 @@ namespace WebElementInterfaceMethodsProperties
             
         }
 
+        [TestMethod]
+        public void WebElementSize()
+        {
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.techexpozed.co.nz";
+
+            Size size = driver.FindElement(By.ClassName("header__logo")).Size;
+            Console.WriteLine(size.Width);
+            Console.WriteLine(size.Height);
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WebElementTagName()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.techexpozed.co.nz";
+
+            var tagName = driver.FindElement(By.ClassName("header__logo")).TagName;
+            Console.WriteLine(tagName);
+
+            //var btn = driver
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WebElemenText()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://techexpozed.co.nz/contact-us.php";
+            var textcontent = driver.FindElement(By.ClassName("heading")).Text;
+            Console.WriteLine(textcontent);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WebElementClear()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            Actions actions = new Actions(driver);
+            driver.Url = "https://www.facebook.com";
+
+            actions.Click(driver.FindElement(By.Id("u_0_o")))
+                .SendKeys("Ana")
+                .Perform();
+            driver.FindElement(By.Id("u_0_o")).Clear();
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WebElementClick()
+        {
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.facebook.com";
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.LinkText("Create a Page")).Click();
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
     }
 }
